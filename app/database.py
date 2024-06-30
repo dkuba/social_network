793c51c47ba124f7372b8e5e9cbff22a0f5f5a27
Dmitriy Kubarev <dkubarev88@gmail.com>
2024-06-30 21:12:06 +0300

    add connection retrying after break

--- app/database.py
+++ app/database.py
@@ -4,30 +4,52 @@ from dotenv import load_dotenv
 
 import psycopg2
 
-
 load_dotenv()
 
-# TODO: восстанавливать соединение при разрыве
-connection = psycopg2.connect(
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    database=os.getenv('DB_NAME')
-)
 
-connection_slave = psycopg2.connect(
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    host=os.getenv('DB_HOST_SLAVE'),
-    port=os.getenv('DB_PORT_SLAVE'),
-    database=os.getenv('DB_NAME')
-)
+def get_conn():
+    return psycopg2.connect(
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        database=os.getenv('DB_NAME')
+    )
+
+
+def get_slave_conn():
+    return psycopg2.connect(
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST_SLAVE'),
+        port=os.getenv('DB_PORT_SLAVE'),
+        database=os.getenv('DB_NAME')
+    )
+
+
+master_conn = get_conn()
+replica_conn = get_slave_conn()
 
 
 def get_db_connection():
-    return connection
+    global master_conn
+    try:
+        cursor = master_conn.cursor()
+        cursor.execute("SELECT 1")
+        cursor.close()
+    except psycopg2.OperationalError:
+        master_conn = get_conn()
+
+    return master_conn
 
 
 def get_db_connection_slave():
-    return connection_slave
+    global replica_conn
+    try:
+        cursor = replica_conn.cursor()
+        cursor.execute("SELECT 1")
+        cursor.close()
+    except psycopg2.OperationalError:
+        replica_conn = get_slave_conn()
+
+    return replica_conn
